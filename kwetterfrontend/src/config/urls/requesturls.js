2e6c8297128defb3f30b40b5942216ebbc72a0b3
imad <imad.elfetouh@hotmail.com>
2021-04-27 12:41:03 +0200

    url

--- kwetterfrontend/src/config/urls/requesturls.js
+++ kwetterfrontend/src/config/urls/requesturls.js
@@ -1,5 +1,4 @@
-// const gateway = 'http://52.158.167.107:8080/'
-const gateway = 'http://localhost:8080/'
+const gateway = 'http://52.158.167.107:8080/'
 
 const urls = {
     signIn: gateway + 'signin',
